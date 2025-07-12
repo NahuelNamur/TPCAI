c29784858c023cba0cbf9f210db9edb9858db336
NahuelNamur <nahuelnamur3@gmail.com>
2025-07-12 09:45:54 -0300

    commit 2 practica
    practica commit

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TPCAI")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afbf830bd03bba11bdd9a4e5f47c82ceb02b33e")]
-[assembly: System.Reflection.AssemblyProductAttribute("TPCAI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TPCAI")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
---
+++ TPCAI/practica1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCai
+{
+    class practical
+    {
+        static void Main(string[] args)
+        {
+            // Call the static DeclararVariables method directly
+            DeclararVariables();
+        }
+
+        // Change 'private void' to 'private static void'
+        private static void DeclararVariables()
+        {
+            string nombre;
+            string apellido;
+
+            nombre = "nahuel";
+            apellido = "namur";
+
+            Console.WriteLine(nombre);
+        }
+    }
+}
+
